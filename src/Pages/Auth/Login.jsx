51bc186fb4295bcc31399180e9a2bdcf982ad642
iinaamasum <iinaamasum@gmail.com>
2022-09-15 14:50:16 +0600

    login page design patially done

--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -2,50 +2,131 @@ import {
   Button,
   Card,
   CardBody,
-  CardFooter,
   CardHeader,
-  Checkbox,
   Input,
   Typography,
 } from '@material-tailwind/react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 export default function Example() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    alert('hi');
+  };
   return (
-    <section className="flex justify-center pt-[96px]">
+    <section className="flex justify-center pt-[96px] px-2">
       <Card className="w-full md:w-[800px] md:px-[32px]">
         <CardHeader
           variant="gradient"
           color="blue"
-          className="mb-4 grid h-28 place-items-center"
+          className="mb-4 flex items-center justify-center h-28 text-center"
         >
-          <Typography variant="h3" color="white">
+          <Typography variant="h3" color="white" className="">
             Welcome to RUET Clearance System
           </Typography>
         </CardHeader>
-        <CardBody className="flex flex-col gap-4">
-          <Input label="Email" size="lg" />
-          <Input label="Password" size="lg" />
-          <div className="-ml-2.5">
-            <Checkbox label="Remember Me" />
-          </div>
-        </CardBody>
-        <CardFooter className="pt-0">
-          <Button variant="gradient" fullWidth>
+
+        <CardBody className="flex flex-col gap-4 w-full md:w-[80%] mx-auto">
+          <Typography
+            variant="h2"
+            color="initial"
+            className="text-center my-2 md:my-4"
+          >
+            Login
+          </Typography>
+          <form id="login-form" onSubmit={handleSubmit(onSubmit)} className="">
+            {/* email section  */}
+            <div className="">
+              <Input
+                label="Email"
+                size="lg"
+                className=""
+                autoComplete="off"
+                {...register('email', {
+                  required: {
+                    value: true,
+                    message: 'Please enter a valid email address.',
+                  },
+                  pattern: {
+                    value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i,
+                    message: 'Invalid Email Provided !!!',
+                  },
+                })}
+              />
+              <label className="text-xs flex my-[2px]">
+                {errors.email?.type === 'required' && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.email.message}
+                  </span>
+                )}
+                {errors.email?.type === 'pattern' && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.email.message}
+                  </span>
+                )}
+              </label>
+            </div>
+            {/* password section  */}
+            <div className="mt-3">
+              <Input
+                label="Password"
+                size="lg"
+                autoComplete="off"
+                {...register('password', {
+                  required: {
+                    value: true,
+                    message: 'Password is required.',
+                  },
+                  pattern: {
+                    value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i,
+                    message: 'Invalid Email Provided !!!',
+                  },
+                })}
+              />
+              <label className="text-xs flex my-[2px]">
+                {errors.password?.type === 'required' && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.password.message}
+                  </span>
+                )}
+                {errors.password?.type === 'pattern' && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.password.message}
+                  </span>
+                )}
+              </label>
+            </div>
+          </form>
+          <Button form="login-form" type="submit" variant="gradient">
             Sign In
           </Button>
-          <Typography variant="small" className="mt-6 flex justify-center">
-            Don't have an account?
-            <Typography
-              as="a"
-              href="#signup"
-              variant="small"
-              color="blue"
-              className="ml-1 font-bold"
-            >
-              Sign up
+          <div className="">
+            <Typography variant="small" className="flex justify-center">
+              Forgot password?
+              <Link
+                to="/reset-password"
+                className="ml-1 font-bold text-blue-600 hover:underline"
+              >
+                Reset Password
+              </Link>
             </Typography>
-          </Typography>
-        </CardFooter>
+            <Typography variant="small" className="flex justify-center">
+              Don't have an account?
+              <Link
+                to="/register"
+                className="ml-1 font-bold text-blue-600 hover:underline"
+              >
+                Register Now
+              </Link>
+            </Typography>
+          </div>
+        </CardBody>
       </Card>
     </section>
   );
